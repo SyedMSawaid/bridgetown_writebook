1a61f5f16f9e1032b92e8773b565556bf66f2f7d
Syed M. Sawaid <34231494+SyedMSawaid@users.noreply.github.com>
2025-08-28 22:05:59 +0200

    changes

--- lib/bridgetown_writebook/commands.rb
+++ lib/bridgetown_writebook/commands.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require_all "bridgetown-core/commands/concerns"
-require "commands/book"
+require_relative "commands/book"
 
 module BridgetownWritebook
   class Commands
